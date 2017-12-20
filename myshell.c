13ba3630359f849708a7ed1591277c76c3707062
Eric Hunzeker <emh128.pitt.edu>
2017-12-19 21:38:13 -0500

    done

--- myshell.c
+++ myshell.c
@@ -3,115 +3,95 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-
-char *delim = " ()<>|&;\n\t";
-
-int get_arglength(char* input)
-{
-    char arguments[100];
-    char* curr;
-    int count = 0;
-
-    strcpy(arguments, input);
-
-    curr = strtok(arguments, delim);
-
-    while(curr != NULL)
-    {
-        count = count + 1;
-        curr = strtok(NULL, delim);
-    }
-    return count;
-}
-
 int main()
 {
     char** inputArray;
-    char* arg;
-    char input[100];
-    char token[100];
-    int inputSize = 0;
+    char* cmd, *curr, *delim = " ()<>|&;\n";
+    char token[100], input[100], arguments[100];
+    int inputSize;
 
-    while(1){
+    while(1)
+    {
         printf("myshell $ ");
-
         fgets(input, 100, stdin);
+        inputSize = 0;
+        strcpy(arguments, input);
+        curr = strtok(arguments, delim);                                                //tokenize arguments using delimiters
 
-        inputSize = get_arglength(input);
+        while(curr != NULL)                                                             //iterate through those arguments to count them
+        {
+            inputSize = inputSize + 1;
+            curr = strtok(NULL, delim);
+        }
 
         inputArray = malloc(sizeof(char*) * (inputSize + 1));
-
         strcpy(token, input);
-
-        arg = strtok(token, delim);
+        cmd = strtok(token, delim);
         int i = 0;
 
-        while(arg != NULL)
+        while(cmd != NULL)
         {
-            inputArray[i] = (char*) malloc(sizeof(char) * strlen(arg));
-            strcpy(inputArray[i], arg);
-            arg = strtok(NULL, delim);
+            inputArray[i] = (char*) malloc(sizeof(char) * strlen(cmd));
+            strcpy(inputArray[i], cmd);
+            cmd = strtok(NULL, delim);
             i++;
         }
 
         inputArray[inputSize] = NULL;
 
-        if(strcmp(inputArray[0], "exit") == 0)
+        if(strcmp(inputArray[0], "exit") == 0)                                          //check if first argument is exit - ignore the rest
             exit(0);
 
-        else if(inputArray[0] == NULL)
+        else if(inputArray[0] == NULL)                                                  //if someone types blank input, continue to new iteration (anti - segmentation fault)
             continue;
-
 
         else if(strcmp(inputArray[0], "cd") == 0)
         {
-            if(chdir(inputArray[1]) == -1)
+            int dir = chdir(inputArray[1]);
+
+            if(dir == -1)
             {
                 if(inputArray[1] == NULL)
                     inputArray[1] = " ";
 
-                printf("cd: %s: No such file or directory\n", inputArray[1]);
+                printf("cd: %s: no such file or directory \n", inputArray[1]);
             }
+        }
+
+        else if(fork() == 0)
+        {
+
+            if(strstr(input, ">") != NULL)
+            {
+                freopen(inputArray[inputSize - 1], "w", stdout);                        //write to filename specified at (inputArray[inputSize-1]
+                free(inputArray[inputSize - 1]);                                        //free inputArray index of filename
+                inputArray[inputSize - 1] = NULL;                                       //set index of filename to null
+            }
+
+            else if(strstr(input, ">>") != NULL)
+            {
+                freopen(inputArray[inputSize - 1], "a", stdout);                        //append to filename specified at (inputArray[inputSize-1]
+                free(inputArray[inputSize - 1]);                                        //free inputArray index of filename
+                inputArray[inputSize - 1] = NULL;                                       //set index of filename to null
+            }
+
+            else if(strstr(input, "<") != NULL)
+            {
+                freopen(inputArray[inputSize - 1], "r", stdin);                         //read from filename specified
+                free(inputArray[inputSize - 1]);                                        //free inputArray at index of filename
+                inputArray[inputSize - 1] = NULL;                                       //set index at filename to null
+            }
+
+            int err = execvp(inputArray[0], inputArray);
+            if(err == -1)
+                printf("-bash : %s: command not found\n", inputArray[0]);
+            exit(0);
         }
 
         else
         {
-            if(fork() == 0)
-            {
-
-                if(strstr(input, ">") != NULL)
-                {
-                    freopen(inputArray[inputSize-1], "w", stdout);
-                    free(inputArray[inputSize-1]);
-                    inputArray[inputSize-1] = NULL;
-                }
-
-                else if(strstr(input, ">>") != NULL)
-                {
-                    freopen(inputArray[inputSize-1], "a", stdout);
-                    free(inputArray[inputSize-1]);
-                    inputArray[inputSize-1] = NULL;
-                }
-
-                else if(strstr(input, "<") != NULL)
-                {
-                    freopen(inputArray[inputSize-1], "r", stdin);
-                    free(inputArray[inputSize-1]);
-                    inputArray[inputSize-1] = NULL;
-                }
-
-                execvp(inputArray[0], inputArray);
-                exit(0);
-            }
-
-            else {
-                int status;
-                wait(&status);
-            }
-
-
+            int status;
+            wait(&status);
         }
     }
 }
-
-
